a1f87fe9c4adab8673596ae791c9b944558abfab
lepht <pete.clark@gmail.com>
2013-01-07 09:33:21 -0500

    Cleanup and comments

--- src/sketch_jan07c.ino
+++ src/sketch_jan07c.ino
@@ -1,58 +1,74 @@
 // #define aref_voltage 3.3
 
 //TODO make output function that checks DEBUG
-int tempPin = 0;    // select the input pin for the potentiometer
+
+// Analog pin number, and input voltage for temperature sensor
+int tempPin = 0;
 float aref_voltage = 5.0;
 
+// Digital pin numbers for status and alert LEDs
 int statusLed = 10;
-
 int alertLed = 7;
+
+// Temperature range for alerts
 float alertLow = 62;
 float alertHigh = 90;
 
 
 void setup() {
+  //TODO switch to calibrated 3.3v for temp sensor
+
+  //// for use with calibrated analog (3.3v) power
   // analogReference(EXTERNAL);
+
+  //configure LEDs for output
   pinMode(statusLed, OUTPUT);
   pinMode(alertLed, OUTPUT);
-  digitalWrite(alertLed, HIGH);
 
+  // Open serial for output
   Serial.begin(9600);
 }
 
 void loop() {
   //TODO make statuLed fade in/out (breathe)
+
+  // Turn on status LED to indivate polling
   digitalWrite(statusLed, HIGH);
 
-  // read the value from the sensor:
+  // read the value from the temp sensor:
   int sensorValue = analogRead(tempPin);
 
   // Serial.print("sensor value: ");
   // Serial.println(sensorValue);
 
+  // Calculate mV from temp sensor output
   float voltage = sensorValue * aref_voltage;
   voltage /= 1024.0; 
 
   // Serial.print("voltage: ");
   // Serial.println(voltage);
 
+  // Convert mV to degrees Celcius and Fahrenheit
   float temperatureC = (voltage - 0.5) * 100 ;
   float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
 
+  //TODO bust temperature bounds checking out into separate function
   if (temperatureF > alertHigh || temperatureF < alertLow) {
     digitalWrite(alertLed, HIGH);
   } else {
     digitalWrite(alertLed, LOW);
   }
 
+  // Output temperature to serial port
   Serial.print("tempF: ");
   Serial.println(temperatureF);
-  
+ 
+  // Wait a sec before turning off status LED
   delay(1000);
   digitalWrite(statusLed, LOW);
+
+  // Wait additional 2secs before re-checking temp
   delay(2000);
-  // stop the program for for <sensorValue> milliseconds:
-//  delay(sensorValue);                  
 }
 
 // vim:ft=arduino
